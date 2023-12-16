a5b5783af5c6f5d3f86c070f394e4a913a8cd640
Andreyjesus <andreyjesus07@hotmail.com>
2023-12-16 01:26:44 -0300

    Completando desafio Dio.me - Criando um Sistema e Abstraindo um Celular com POO em C#

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Console.WriteLine("Smartphone Nokia:");
+
+Smartphone nokia = new Nokia(numero: "(11) 9 8183-4664", modelo: "Nokia-A4", imei: "696969", memoria: 120);
+nokia.Ligar();
+nokia.InstalarAplicativo("Aplicativo Dio.me");
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+
+Smartphone iphone = new Iphone(numero: "(11) 9 8183-4664", modelo: "Iphone-S7", imei: "90909898", memoria: 300);
+iphone.Ligar();
+iphone.InstalarAplicativo("Aplicativo Dio.me");
